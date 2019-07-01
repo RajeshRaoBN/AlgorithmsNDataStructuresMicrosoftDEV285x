56205975a019f32e4f1d171163d145126befee4c
Rajesh Narayana Rao <mailrbn@gmail.com>
2019-07-01 10:23:39 +1000

    1Jul2019

---
+++ DictPractice2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 13 18:27:06 2019
+
+@author: rajeshnarayanarao
+"""
+
+AFL_team = {
+        'Footscray'     : 'Buldogs',
+        'Melbourne'     : 'Deamons',
+        'St. Kilida'    : 'Saints',
+        'Sydney'        : 'Swans',
+        'Essendon'      : 'Bombers'
+        }
+
+AFL_team['North Melbourne'] = 'Kangaroos'
